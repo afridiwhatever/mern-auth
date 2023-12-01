30469a108889372d005e29d37aaf48d3e4f412ba
Afridi <afridiwhatever@gmail.com>
2023-12-02 05:41:43 +0600

    Added Sign in API Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -20,3 +20,37 @@ export const signup = async (req, res, next) => {
     );
   }
 };
+
+export const signin = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  if (!username && !email) {
+    res.status(400).json({ message: "Username or email is required" });
+    return;
+  }
+  try {
+    const foundUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (!foundUser) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+    const isPasswordValid = await bcryptjs.compare(
+      password,
+      foundUser.password
+    );
+    if (isPasswordValid) {
+      res.status(201).json({ message: "Succesful" });
+      return;
+    } else {
+      res.status(401).json({ message: "Username of password is wrong" });
+      return;
+    }
+  } catch (error) {
+    next(
+      errorCreator(
+        400,
+        `Failed to log in, Reason: ${error.message || "Internal Server Error"}`
+      )
+    );
+  }
+};
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,25 +1,60 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Signup = () => {
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setUserData((prev) => {
+      return {
+        ...prev,
+        [id]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3000/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    const data = await response.json();
+  };
+
   return (
     <div className="max-w-xl mx-auto p-3">
       <h1 className="text-3xl font-semibold text-center my-7">Sign Up</h1>
 
-      <form className="flex flex-col gap-4 px-8">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 px-8">
         <input
           className="bg-slate-100 rounded-lg p-3"
           type="text"
           placeholder="Username"
+          id="username"
+          onChange={handleChange}
         />
         <input
           className="bg-slate-100  rounded-lg p-3"
           type="email"
           placeholder="Email"
+          id="email"
+          onChange={handleChange}
         />
         <input
           className="bg-slate-100  rounded-lg p-3"
           type="password"
           placeholder="Password"
+          id="password"
+          onChange={handleChange}
         />
         <button className="bg-slate-700 text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-70 disabled:cursor-not-allowed">
           SIGN UP
